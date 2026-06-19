11db9c6317d9c98ecb9ff0d662638af17821250b
andre luis <andrelsoaresf@gmail.com>
2026-06-19 20:41:04 -0300

    separar a função das cores do main.py
    Co-authored-by: Helena Couto <helenacdos00@gmail.com>
    Co-authored-by: Icaro Eduardo <icaroeduardodesouzalucena@gmail.com>

---
+++ Cores.py
@@ -0,0 +1,14 @@
+def cores(cor):
+    CORES = {
+        'verde': (0, 255, 0),
+        'amarelo': (255, 255, 0),
+        'laranja': (255, 165, 0),
+        'vermelho': (255, 0, 0),
+        'cinza': (50, 50, 50),
+        'branco': (255, 255, 255),
+        'preto': (0, 0, 0),
+        'azul': (81, 172, 255),
+        'marrom': (63, 38, 13),
+        'roxo': (171, 102, 234)
+    }
+    return CORES[cor]
--- main.py
+++ main.py
@@ -1,20 +1,6 @@
 import pygame
+from Cores import cores
 from random import randint
-
-def cores(cor):
-    cores = {
-        'verde': (0, 255, 0),
-        'amarelo': (255, 255, 0),
-        'laranja': (255, 165, 0),
-        'vermelho': (255, 0, 0),
-        'cinza': (50, 50, 50),
-        'branco': (255, 255, 255),
-        'preto': (0, 0, 0),
-        'azul': (81, 172, 255),
-        'marrom': (63, 38, 13),
-        'roxo': (171, 102, 234)
-    }
-    return cores[cor]
 
 def vida_dragao(janela, hp_dragao):
     pygame.draw.rect(janela, cores('preto'), (795, 95, 460, 50))
